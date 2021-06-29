63b1d8af36661ed85ae2da22b349ea004c9add40
Dayana Alejandro <da2410@nyu.edu>
2021-06-29 18:18:09 +0000

    initial commit

---
+++ filename.py
@@ -0,0 +1,36 @@
+import spotipy
+import requests
+import json
+'''
+url = 'https://api.spotify.com/v1/audio-analysis/4JpKVNYnVcJ8tuMKjAj50A'
+headers = {'Authorization': 'Bearer TOKEN-HERE'}
+r = requests.get(url, headers=headers)
+print( r.json())
+
+'''
+CLIENT_ID = '74a90350ed1d4c1dbc80fe0dc996ce2d'
+CLIENT_SECRET = '9f31a864b9f14ceab01376dcfdb8c730'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+print(auth_response.status_code)
+
+auth_response_data = auth_response.json()
+
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+BASE_URL = 'https://api.spotify.com/v1/'
+track_id = '6mFkJmJqdDVQ1REhVfGgd1'
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+
+r = r.json()
+print(r)
