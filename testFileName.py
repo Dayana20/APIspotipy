33aaa8ea6b421ddbcd3c6cb8d08a19d6ab206acc
Dayana Alejandro <da2410@nyu.edu>
2021-07-01 19:24:05 +0000

    Add existing

--- testFileName.py
+++ testFileName.py
@@ -5,7 +5,7 @@ from filename import setting_up, calling
 
 class TestFileName(unittest.TestCase):
     def test_function1(self):
-        self.assertEqual(setting_up(1), 0)
+        self.assertEqual(setting_up(1) , 0)
 
     def test_function2(self):
         self.assertEqual(calling(2, 1), 3)
