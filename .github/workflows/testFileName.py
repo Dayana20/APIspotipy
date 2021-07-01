464fd9464725a5761176cdd4ca2d89d601c22e26
Dayana Alejandro <39806222+Dayana20@users.noreply.github.com>
2021-07-01 16:35:45 -0400

    Create testFileName.py

---
+++ .github/workflows/testFileName.py
@@ -0,0 +1,15 @@
+import unittest
+from filename import setting_up, calling
+
+
+class TestFileName(unittest.TestCase):
+    def test_function1(self):
+        self.assertEqual(setting_up(1), 0)
+
+    def test_function2(self):
+        self.assertEqual(calling(2, 1), 3)
+        self.assertEqual(calling(2.1, 1.2), 3.3)
+
+
+if __name__ == '__main__':
+    unittest.main()
